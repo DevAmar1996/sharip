5ee0d3ccee080f4aadbe604acc9b3b3ef1a7d9dc
DevAmar <amaramassi1996@gmail.com>
2018-04-20 15:45:32 +0300

    Initial Commit

---
+++ Sharp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sharp
+//
+//  Created by Amar Amassi  on 4/20/18.
+//  Copyright © 2018 Amar Amassi . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
